0e2b76462a8f90be638c10c99acdbb055032155e
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-06-25 22:23:17 +0300

    feat: user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,9 +27,14 @@ export const register = async (req, res) => {
             picturePath,
             friends,
             location,
-            occupation
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
         })
-    } catch (err) {
 
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
     }
 }
